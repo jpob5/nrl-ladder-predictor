3b9962e8d3919a7bca1a63642e679d581174b15d
jpob5 <jacobfife@gmail.com>
2019-08-15 11:24:39 +1000

    deploy

--- build/precache-manifest.18a92ea70c0546935c281a6dd60b4e67.js
+++ build/precache-manifest.18a92ea70c0546935c281a6dd60b4e67.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c90a49c7f378d78937ec85caba911cb1",
+    "revision": "a7249fcb9de3bf1cf7e73d007f9323c7",
     "url": "/nrl-ladder-predictor/index.html"
   },
   {
-    "revision": "382a42bfe6f6807ec8c3",
+    "revision": "19f34e7f90a44f7c16e7",
     "url": "/nrl-ladder-predictor/static/css/main.88f5c9cd.chunk.css"
   },
   {
-    "revision": "4934003cae2ddd5c1b9a",
-    "url": "/nrl-ladder-predictor/static/js/2.754d80a7.chunk.js"
+    "revision": "b2cc9c32289a2b27ec14",
+    "url": "/nrl-ladder-predictor/static/js/2.6747dcae.chunk.js"
   },
   {
-    "revision": "382a42bfe6f6807ec8c3",
-    "url": "/nrl-ladder-predictor/static/js/main.bf9203d3.chunk.js"
+    "revision": "19f34e7f90a44f7c16e7",
+    "url": "/nrl-ladder-predictor/static/js/main.c868e9a7.chunk.js"
   },
   {
     "revision": "9289f1800e7a86866de0",
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Live from './components/Live'
 import './styles/styles.scss';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
 	return (
@@ -8,7 +9,8 @@ function App() {
 			<link href="https://fonts.googleapis.com/css?family=Oxygen&display=swap" rel="stylesheet"></link>
 			<link href="https://fonts.googleapis.com/css?family=Cutive+Mono&display=swap" rel="stylesheet"></link>
 			<div className="App">
-				<Live />
+				<Route exact path={'/'} render={() => <Live/>} />
+				{/* <Live /> */}
 			</div>
 		</>
 	);
