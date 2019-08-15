f956b5fef24c1de04c5e9f2ad0d6eb92612aac69
jpob5 <jacobfife@gmail.com>
2019-08-15 11:27:57 +1000

    Updates

--- precache-manifest.f695ef86e88d724add91ef51943ce476.js
+++ precache-manifest.f695ef86e88d724add91ef51943ce476.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a7249fcb9de3bf1cf7e73d007f9323c7",
+    "revision": "2017e1c7fea95c3e7a3b1692b6e6e050",
     "url": "/nrl-ladder-predictor/index.html"
   },
   {
-    "revision": "19f34e7f90a44f7c16e7",
+    "revision": "ece8adcd636ecbc89d21",
     "url": "/nrl-ladder-predictor/static/css/main.88f5c9cd.chunk.css"
   },
   {
-    "revision": "b2cc9c32289a2b27ec14",
-    "url": "/nrl-ladder-predictor/static/js/2.6747dcae.chunk.js"
+    "revision": "3281f5bf8bd3b2d5dbf5",
+    "url": "/nrl-ladder-predictor/static/js/2.d16f0422.chunk.js"
   },
   {
-    "revision": "19f34e7f90a44f7c16e7",
-    "url": "/nrl-ladder-predictor/static/js/main.c868e9a7.chunk.js"
+    "revision": "ece8adcd636ecbc89d21",
+    "url": "/nrl-ladder-predictor/static/js/main.6c8b1b61.chunk.js"
   },
   {
     "revision": "9289f1800e7a86866de0",
