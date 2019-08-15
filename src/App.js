77a0cbe70a747c5bdefbbf865302981fc8e9f0e2
jpob5 <jacobfife@gmail.com>
2019-08-15 11:32:19 +1000

    deploy

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Live from './components/Live'
 import './styles/styles.scss';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 
 function App() {
 	return (
 		<>
 			<link href="https://fonts.googleapis.com/css?family=Oxygen&display=swap" rel="stylesheet"></link>
 			<link href="https://fonts.googleapis.com/css?family=Cutive+Mono&display=swap" rel="stylesheet"></link>
-			<div className="App">
-				<Route exact path={'/'} render={() => <Live/>} />
-				{/* <Live /> */}
-			</div>
+			<HashRouter basename="/">
+				<div className="App">
+					<Route exact path={'/'} component={Live} />
+					{/* <Live /> */}
+				</div>
+			</HashRouter>
 		</>
 	);
 }
